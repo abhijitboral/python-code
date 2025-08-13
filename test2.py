1a3e0cbd26ce6e8616d0b56436fe7c1920251009
abhijitboral <abhijit1512.boral@gmail.com>
2025-08-13 11:39:42 +0530

    prectice in test and test2 file

--- test2.py
+++ test2.py
@@ -27,7 +27,32 @@ hof = heigher_order_func('func1')
 print(hof('Raju'))
 
 # Example of filter function
-
 list_data = [22,23,24,25,26,27,28]
 filter_obj = filter(lambda num:True if num%2 == 0 else False,list_data)
-print(list(filter_obj))
+print(list(filter_obj)) 
+
+# second example of filter function
+str_input = 'abhijit'
+vowels = ['a','e','i','o','u']
+vowels_letters = filter(lambda letter:True if letter in vowels else False,str_input)
+print("Vowels: ",list(vowels_letters))
+
+# Filter the student name whos marks is getter than 50
+
+student_data = {
+    'sachin':100,
+    'virat':51,
+    'Rahul':55,
+    'Pant':20,
+    'jadeja':78,
+    'aswin':49
+}
+def func(student):
+    return student_data[student] >= 50
+filtered_list = filter(func,student_data)
+print('Marks getter than 50: ',list(filtered_list))
+
+# Example of map function
+list_data = [22,23,24,25,26,27,28]
+map_obj = map(lambda num:num*2,list_data)
+print(list(map_obj))
